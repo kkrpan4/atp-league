6fbedc63d77bab3c2ba17bc8c827885d0e2c2f5f
Karlo <158114442+kkrpan4@users.noreply.github.com>
2026-02-16 19:36:07 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,18 @@
+const playersData = [
+    { rank: 1, name: "Jannik Sinner", points: 9500, tournaments: 18, winRate: "85%", image: "https://s7.ezgif.com/tmp/ezgif-7cb483b8b95af448.jpg" },
+    { rank: 2, name: "Novak Đoković", points: 8710, tournaments: 15, winRate: "82%", image: "https://live-production.wcms.abc-cdn.net.au/e7acf4022ca543dbb4fb936acbe1cda1?impolicy=wcms_crop_resize&cropH=2813&cropW=5000&xPos=0&yPos=163&width=862&height=485" },
+    { rank: 3, name: "Carlos Alcaraz", points: 8635, tournaments: 20, winRate: "80%", image: "https://r.testifier.nl/Acbs8526SDKI/resizing_type:fit/watermark:IMAGO%20%2F%20ABACAPRESS/width:3840/height:2560/plain/https://s3-newsifier.ams3.digitaloceanspaces.com/tennisinfinitycom/images/2025-01/alcaraz-carlos-australianopen25-imagoabacapress7-679208779537e.jpg@webp" },
+    { rank: 4, name: "Daniil Medvedev", points: 7165, tournaments: 22, winRate: "78%", image: "https://images.unsplash.com/photo-1588615419957-bf66d53c6b49?auto=format&fit=crop&q=80&w=400" },
+    { rank: 5, name: "Alexander Zverev", points: 6885, tournaments: 24, winRate: "75%", image: "https://images.unsplash.com/photo-1622279457486-62dcc4a4bd13?auto=format&fit=crop&q=80&w=400" },
+    { rank: 6, name: "Andrey Rublev", points: 4830, tournaments: 25, winRate: "70%", image: "https://images.unsplash.com/photo-1595435934249-5df7ed86e1c0?auto=format&fit=crop&q=80&w=400" },
+    { rank: 7, name: "Holger Rune", points: 3825, tournaments: 26, winRate: "68%", image: "https://images.unsplash.com/photo-1542144557-f5bc8fc905f8?auto=format&fit=crop&q=80&w=400" },
+    { rank: 8, name: "Hubert Hurkacz", points: 3540, tournaments: 23, winRate: "66%", image: "https://images.unsplash.com/photo-1554068865-24cecd4e34b8?auto=format&fit=crop&q=80&w=400" }
+];
+
+const tournamentsData = [
+    { date: "Ožujak 2026", name: "Indian Wells Masters", location: "USA", category: "Masters 1000" },
+    { date: "Travanj 2026", name: "Miami Open", location: "USA", category: "Masters 1000" },
+    { date: "Svibanj 2026", name: "Roland Garros", location: "Francuska", category: "Grand Slam" },
+    { date: "Srpanj 2026", name: "Wimbledon", location: "UK", category: "Grand Slam" },
+    { date: "Kolovoz 2026", name: "US Open", location: "USA", category: "Grand Slam" }
+];
